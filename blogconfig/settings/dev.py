bc07c3a6093f97c9b6a4aaffb877f960ec8efe2f
Ayodimeji Kadri <ayodimeji1@hotmail.com>
2022-02-02 22:49:23 +0100

    fix static issues

--- blogconfig/settings/dev.py
+++ blogconfig/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 SECRET_KEY = 'django-insecure-4x&ys-hp(wqveql+%48p_i0rbr+*&9fi$dr9nf2^@3kgzp(*h&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
