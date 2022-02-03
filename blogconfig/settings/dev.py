f229018999b8c8c66ebe4a031e6679e6b4ffba6b
Ayodimeji Kadri <ayodimeji1@hotmail.com>
2022-02-03 10:35:56 +0100

    collecting static

--- blogconfig/settings/dev.py
+++ blogconfig/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 SECRET_KEY = 'django-insecure-4x&ys-hp(wqveql+%48p_i0rbr+*&9fi$dr9nf2^@3kgzp(*h&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -15,7 +15,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'),
-] 
+]  
 
 MEDIA_URL = 'media/'
 
